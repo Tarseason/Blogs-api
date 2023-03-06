8fce522a3402d3b9648087ff28e6aa3137b1233b
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2023-03-06 16:00:24 -0400

    Conclui requisito N°2

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      display_name: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      image: {
+        type: DataTypes.STRING
+      },
+    }
+  )
+  return User
+}
